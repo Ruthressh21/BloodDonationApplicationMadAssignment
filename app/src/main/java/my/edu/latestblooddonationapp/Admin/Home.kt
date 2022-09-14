06360907963758448ce49ecdbc14d4596cdd8478
Haode-012 <62163443+Haode-012@users.noreply.github.com>
2022-09-14 12:49:44 +0800

    commit message

---
+++ app/src/main/java/my/edu/latestblooddonationapp/Admin/Home.kt
@@ -0,0 +1,40 @@
+package my.edu.latestblooddonationapp.Admin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import my.edu.latestblooddonationapp.R
+import my.edu.latestblooddonationapp.databinding.FragmentAdminHomeBinding
+
+class Home : Fragment() {
+
+    private var _binding: FragmentAdminHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentAdminHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
