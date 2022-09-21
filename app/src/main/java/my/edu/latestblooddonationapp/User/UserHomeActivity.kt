d0052c1e104c28f937b5b4f964b06eefdde48139
Haode-012 <62163443+Haode-012@users.noreply.github.com>
2022-09-21 15:30:16 +0800

    haode commit

--- app/src/main/java/my/edu/latestblooddonationapp/User/UserHomeActivity.kt
+++ app/src/main/java/my/edu/latestblooddonationapp/User/UserHomeActivity.kt
@@ -18,22 +18,57 @@ import my.edu.latestblooddonationapp.databinding.ActivityHomeUserBinding
 
 class UserHomeActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityHomeUserBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeUserBinding.inflate(layoutInflater)
 
+        binding = ActivityHomeUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.appBarHomeUser.toolbar)
 
-      binding.buttonClick.setOnClickListener{
-          val intent = Intent(this,DonorVerifyRequest::class.java)
-          startActivity(intent)
-      }
-
+        val drawerLayout: DrawerLayout = binding.drawerLayoutUser
+        val navView: NavigationView = binding.navViewUser
+        val navController = findNavController(R.id.nav_host_fragment_content_home_user)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,
+                R.id.nav_editProfile,
+                R.id.nav_aboutUs,
+                R.id.action_logOut
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return when (item.itemId) {
+            R.id.action_logOut -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_home_user)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
 }
