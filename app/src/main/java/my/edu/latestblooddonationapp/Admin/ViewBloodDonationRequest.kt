5b25e2768697765924e597f32c8ea1debb897b3d
Haode-012 <62163443+Haode-012@users.noreply.github.com>
2022-09-25 22:11:26 +0800

    haode commit

--- app/src/main/java/my/edu/latestblooddonationapp/Admin/ViewBloodDonationRequest.kt
+++ app/src/main/java/my/edu/latestblooddonationapp/Admin/ViewBloodDonationRequest.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -40,6 +41,7 @@ class ViewBloodDonationRequest : Fragment() {
     ): View {
 
         _binding = FragmentViewBloodDonationRequestBinding.inflate(inflater, container, false)
+
 
         firebaseAuth = FirebaseAuth.getInstance()
         loadCategories()
